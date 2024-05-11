2eefff23e56ae8d076164790d5186158ca612adc
Benjamin Rühl <dev.ruehl@gmail.com>
2024-05-11 11:33:22 +0200

    build: setup gh-pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,7 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://benruehl.github.io',
+  base: '/astro-katzenbetreuung',
   integrations: [icon()]
 });
